2a26af3ec0981d845fd526ac917e03dbcbd1d221
SaraHanyAbdelhady <160270735+SaraHanyAbdelhady@users.noreply.github.com>
2024-04-25 02:18:42 +0200

    Table class

---
+++ Restaurant/src/Restaurant.java
@@ -0,0 +1,14 @@
+
+public class Restaurant {
+	public static void main(String[] args)
+    {
+        Table t1 = new Table (1,3);
+        Table t2 = new Table (2,4);
+        Table t3 = new Table (3,5);
+        Table t4 = new Table (4,7);
+        Table t5 = new Table (5,5);
+        Table t6 = new Table (6,6);
+        Table t7 = new Table (7,4);
+        Table t8 = new Table (8,3);
+    }   
+}
---
+++ Restaurant/src/Table.java
@@ -0,0 +1,48 @@
+
+public class Table {
+	private int tableNo;
+	private int numberOfSeats;
+	private Boolean isAvailable;
+	
+	public Table(int tableNo, int numberOfSeats)
+	{
+		this.tableNo = tableNo;
+		this.numberOfSeats = numberOfSeats;
+		this.isAvailable = true;
+	}
+	public int getTableNo() {
+		return tableNo;
+	}
+	public void setTableNo(int tableNo) {
+		this.tableNo = tableNo;
+	}
+	public int getNumberOfSeats() {
+		return numberOfSeats;
+	}
+	public void setNumberOfSeats(int numberOfSeats) {
+		this.numberOfSeats = numberOfSeats;
+	}
+	public Boolean getIsAvailable() {
+		return isAvailable;
+	}
+	public void setIsAvailable(Boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	public void bookTable(int tableNo, int numberOfSeats)
+	{
+		if (isAvailable == false || this.numberOfSeats < numberOfSeats)
+		{
+			System.out.println("Sorry, this table is not available for now, Choose another table.");
+		}
+		else 
+		{
+			System.out.println("Your table is ready, Bon appetit :)");
+			this.isAvailable = false;
+		}
+		
+		
+	}
+	
+	
+	
+}
