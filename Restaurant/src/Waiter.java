e514f572889b17b301102a70551b141e0ec6172a
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2024-05-01 01:24:59 +0300

    Update Waiter.java

--- Restaurant/src/Waiter.java
+++ Restaurant/src/Waiter.java
@@ -1,8 +1,48 @@
+import java.util.List;
 
-public class Waiter {
+public class Waiter extends Person{
+	private static int waiterIdAdder=1;
+	private int employeeId;
+	private List<Table> assignedTables; //t2rebn keda lazm a5od ids mo5tlfa
+	// private List <Order> order;  msh mot5yla ha3ml feeh eh
+	private MenuItems menu;
+   
 
-	public Waiter() {
-		// TODO Auto-generated constructor stub
+
+	public Waiter(String name,String num,String email) {
+		super(name,num,email);
+		this.employeeId = waiterIdAdder++;
 	}
+	 public void setTable(List<Table> table) {
+	        this.assignedTables = table;
+	    }
 
+	    public List<Table> getTable() {
+	        return this.assignedTables; //hatrg3 el array?
+	    }
+	 public int getID()
+	 {
+		 return this.employeeId;
+	 }
+//	    public void setOrder(List<Order> order) {
+//     this.order = order;
+// }
+// public List<Order> getOrder() {
+//     return this.order;
+// }
+	 
+	 
+	 
+//	 public void takeOrder(Order order) {
+//	     
+//	    }
+//	    public void serveOrder(Order order) {	        
+//	    }
+//	    public void processPayment(Order order) {    
+//	    }
+
+	@Override
+	public  String identifyRole() {
+		return "Waiter ID" + this.employeeId;
+	}
 }
