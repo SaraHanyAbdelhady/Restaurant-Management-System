4351c9f7f24a79a43960b0a6daa5ff32d986a1e9
Marina-Mokhtar <2100929@eng.asu.edu.eg>
2024-05-02 22:06:34 +0200

    Update Order.java

--- Restaurant/src/Order.java
+++ Restaurant/src/Order.java
@@ -1,44 +1,47 @@
 import java.util.*;
+import java.util.ArrayList;
 
-public class Order {
- 
+public class order extends MenuItems {
+	
+    private static int numberOfOrders;
+    private ArrayList<MenuItems> order= new ArrayList<MenuItems>() ;
+
     
-	public Order() {
-		//table no m3a el table
-		System.out.println("If you want to know our offers Enter yes");
-		System.out.println("If you don't Enter no");
-		Scanner input1 = new Scanner(System.in);
-		String answer1 = input1.nextLine();
-		if (answer1=="yes")
-		{
-			//go to offers
-		}
-		else if (answer1=="no"){
-		System.out.println("To get your order ");
-		System.out.println("If you want MainDishes enter 1");
-		System.out.println("If you want Desserts enter 2");
-		System.out.println("If you want Beverages enter 3");
-		getorder ();
-		}
+	public order() {
+		
 	}
-	public void getorder () {
-		Scanner input2 = new Scanner(System.in);
-		int answer2 = input2.nextInt();
-		if (answer2==1)
-		{
-			//go to MainDishes
-		}
-		else if (answer2==2)
-		{
-			//go to Desserts
-		}
-		else if (answer2==3)
-		{
-			//go to Beverages
-		}
-		else
-		{
-			System.out.println("please enter 1 or 2 or 3");
-		}
-	}	      
+	
+	public void addItem (MenuItems item) {
+		order.add(item);	
+	}
+	
+	public void removeItem (MenuItems item) {
+		order.remove(item);	
+	}
+    public void clearOrder() {
+    	order.clear();
+    }
+	
+    public int getSize() {
+		return order.size();
+    }
+    
+    
+    public int getNumber() {
+		return numberOfOrders ;
+    } 
+    
+    public void incrementNumberOfOrders() {
+    	numberOfOrders++;
+    }
+    
+    
+	
+	
+	
+	@Override
+	public String description(MenuItems I) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
