cd80feef708b2f93217568f9dcd30759194b92ef
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-05-04 20:07:20 +0300

    changes

--- Restaurant/src/Restaurant.java
+++ Restaurant/src/Restaurant.java
@@ -1,4 +1,7 @@
 //gui hna
+
+import java.util.ArrayList;
+
 public class Restaurant {
 	public static void main(String[] args)
     {
@@ -8,5 +11,8 @@ public class Restaurant {
         MainDishes.initMainDishes();
         Desserts.initDesserts();
         Beverages.initBeverages();
+        
+        // sorted list
+        ArrayList<MainDishes> sorted = MainDishes.sortedMainDishesItems;
     }   
 }
