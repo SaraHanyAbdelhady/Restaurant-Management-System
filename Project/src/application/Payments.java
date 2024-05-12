9592ed2fee9085bf9eba63e9f63c0feb60448d9c
Takwa Madkour <takwamadkour24@gmail.com>
2024-05-13 01:13:38 +0300

    GUI of payment

--- Project/src/application/Payments.java
+++ Project/src/application/Payments.java
@@ -1,5 +1,15 @@
-package application;
+
 //t2rebn 5lsan
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class Payments {
 	//private int tableNumber;
@@ -11,25 +21,51 @@ public class Payments {
 	Payments(){
 		
 	}
-//	public void setBillValueAndTableNo(double value, int tableNo) {
-//		if(value > 0 && tableNo>0 && tableNo <9) {
-//			billValue=value;
-//		    tableNumber=tableNo;
-//		}
-//		else
-//			System.out.println("Check your order with the waiter.");
-//	}
-	/*public void setTableNumber(int tableNo) {
-		if(tableNo>0 && tableNo<10)
-			tableNumber=tableNo;
-		else
-			System.out.println("Check your order with the waiter.");
-	}*/
+
 	public double getBillValue(Order order ,Table table) {
 		 table.setIsAvailable(true);
 		return order.getOrderPrice();	
 	}
-  
+	
+	
+	public class PAYMENT extends Application {
+		@Override
+		public void start(stage primaryStage) {
+			BorderPane pane =new BorderPane();
+			// placing the nodes in the pane
+			pane.setTop(getHBox());
+			pane.setLeft(getVBox());
+			// EventHandling
+		    cashButton.setOnAction(e->{
+		    	pane.setCenter(new Label("your bill value is" + getBillValue));});
+		    creditButton.setOnAction(e->{
+		    	pane.setCenter(new Label("your bill value is" + (getBillValue+ getBillValue*0.01)));});	
+			
+			// create Scene and place it in the stage
+			Scene scene= new Scene(pane);
+			primaryStage.setTitle("Payment");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+
+	}
+
+	private HBox getHBox() {
+		HBox hBox= new HBox(15);
+		hBox.setPadding(new Insets(15,15,15,15));
+		hbox.getChildren().add(new Label("How would you like to pay ?"));
+		return hBox;
+	}
+
+	private VBox getVBox() {
+		VBox vBox= new VBox(15);
+		vBox.setPadding(new Insets(15,5,5,5));
+		Button cashButton= new Button("Cash");
+		Button creditButton= new Button("Credit");
+		vBox.getChildern().addAll(cashButton,creditButton);
+		return vBox;
+		
+	}
 	
 	
 }
