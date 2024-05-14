dcbc808c73eeee4c50b57b253bc9ec589bdd001c
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-05-14 03:58:49 +0300

    Changing some variable names

--- Project/src/application/Customer.java
+++ Project/src/application/Customer.java
@@ -4,45 +4,45 @@ package application;
 public class Customer extends Person {
 	
 	private static int customerIdAdder=1;
-    private int customer_ID;
-    private int loyalty_points=0;
+    private int customerId;
+    private int loyaltyPoints=0;
     
     public Customer() {}
     
 	public Customer(String name,String num) {
 		super(name,num);
-		this.customer_ID = customerIdAdder++;
+		this.customerId = customerIdAdder++;
 	}
     
     
 	public Customer(String name,String num,String email) {
 		super(name,num,email);
-		this.customer_ID = customerIdAdder++;
+		this.customerId = customerIdAdder++;
 	}
 	public void setCustomerIdAdder(int customerIdAdder) {
 		Customer.customerIdAdder= customerIdAdder;
 	}
 	public void setPoints(int Points) { 
-		this.loyalty_points=Points;
+		this.loyaltyPoints=Points;
 	}
 
 	 public int getID()
 	 {
-		 return this.customer_ID;
+		 return this.customerId;
 	 }
 	 public int getLoyalty_points()
 	 {
-		 return this.loyalty_points;
+		 return this.loyaltyPoints;
 	 }
 	 
 	public void addLoyaltyPoints(int Points) {   //lw 3rfna 3la 7asab el fatoora momken ad5l points 
 		//for every order made by the customer there is a 50 loyalty points  added to his profile
-		this.loyalty_points+=Points;
+		this.loyaltyPoints+=Points;
 	}
 	
 @Override
 public  String identifyRole() {
-	return "Customer"+ this.customer_ID;
+	return "Customer"+ this.customerId;
 }
 
 }
